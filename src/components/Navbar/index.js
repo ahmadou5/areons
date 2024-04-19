02404891d47a0982780425a9079f177c78833e9e
ahmadou5 <ahmadlasauwal@gmail.com>
2024-04-19 18:46:14 +0100

    new

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -19,8 +19,8 @@ export const Navbar2 = () => {
     <nav className="w-full px-2 z-0 mt-3 fixed inset-x-0 top-2 flex justify-center items-center">
         <div className="w-[98%]  backdrop-blur-lg bg-clip-padding bg-[#171717]  border border-blue-600/25 bg-opacity-60 md:w-880 p-4 rounded-lg flex items-center">
           <div className="flex py-0.5 px-2">
-            <img src="./assets/areonLogo.png" className=" h-8 mt-0.5 font-medium"/>
-            <p className="ml-0.5 text-3xl text-white/55 font-extralight">Terminal</p>
+            {/**<img src="./assets/areonLogo.png" className=" h-8 mt-0.5 font-medium"/>**/}
+            <p className="ml-0.5 text-3xl text-white/55 font-extralight">Zodiac info</p>
           </div>
          
           <div className="block md:hidden ml-auto cursor-pointer" onClick={()=> setisActive(!isActive)}>
