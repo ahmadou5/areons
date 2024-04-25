79ba262aa0c051bbfa5a76e53ee8a3bfc5e3d449
ahmadou5 <ahmadlasauwal@gmail.com>
2024-04-25 10:48:59 +0100

    chore:40

--- src/components/Tokens/index.js
+++ src/components/Tokens/index.js
@@ -3,7 +3,8 @@ import { TokensCard } from "./components/TokensCard"
 export const Tokens  = () => {
     return ( 
     <div className="w-full">
-       <TokensCard />
+        <p className="text-center py-2 px-3 text-xl">No Data Coming Soon</p> 
+       {/** <TokensCard /> **/}
     </div>
     )
 }
